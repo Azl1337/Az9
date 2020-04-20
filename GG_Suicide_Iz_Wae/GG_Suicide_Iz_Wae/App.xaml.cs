5f8bf7bf6b5c792803fba27c5a838f15f166f61c
Azl1337 <Azl1337@AZL1337>
2020-04-20 18:07:49 +0300

    Добавили формы

---
+++ GG_Suicide_Iz_Wae/GG_Suicide_Iz_Wae/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GG_Suicide_Iz_Wae
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ GG_Suicide_Iz_Wae/GG_Suicide_Iz_Wae/Pages/RegisterAsRunnerPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GG_Suicide_Iz_Wae.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RegisterAsRunnerPage.xaml
+    /// </summary>
+    public partial class RegisterAsRunnerPage : Window
+    {
+        public RegisterAsRunnerPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
